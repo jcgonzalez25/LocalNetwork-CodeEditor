aa687fa7b989349854740bcf3f5e502eb79d11a3
JC Gonzalez <devgonzalez25@gmail.com>
2019-09-21 06:16:22 -0500

    adding setup script that will be called by npm run setup and will update the client_config.js file to the iphost location for websocket.

---
+++ setup.js
@@ -0,0 +1,37 @@
+'use strict';
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var fs = require('fs');
+function getIP(){
+    return new Promise(res=>{
+        Object.keys(ifaces).forEach(function (ifname) {
+            var alias = 0;
+            var ip;
+            ifaces[ifname].forEach(function (iface) {
+              if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+              }
+          
+              if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+              } else {
+                // this interface has only one ipv4 adress
+                ip = iface.address;
+                res(ip)
+              }
+              ++alias;
+            });
+          
+          });
+    
+    })
+}
+getIP().then(IP=>{
+    fs.writeFile('client_config.js',
+    `const LOCAL_SERVER_IP = \"${IP}\";\nconst PORT=\"6000\";`,
+    (err)=>console.log(err)
+    )
+})
+
